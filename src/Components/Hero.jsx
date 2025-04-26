481e9ffabada394394e28e3b651a0364d341585a
Sachin Vishwakarma <sachinvishwakarma2215@gmaail.com>
2025-04-26 22:11:57 +0530

    video updated

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -4,16 +4,19 @@ function Hero() {
   return (
     <div>
       <div className="flex items-center justify-center">
-        <div className="w-full bg-red-500 h-96">
-          <video
-            autoPlay
-            loop
-            muted
-            className="w-full h-full object-cover object-center"
-            src="src/assets/LongVideos/Poster.mp4"
-          ></video>
+        <div className="w-full bg-red-500 h-96 overflow-hidden">
+          <iframe
+            className="w-full h-full"
+            src="https://www.youtube.com/embed/sZamsqQdYWU?autoplay=1&mute=1&controls=0&showinfo=0&rel=0&loop=1&playlist=sZamsqQdYWU"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerPolicy="strict-origin-when-cross-origin"
+            allowFullScreen
+          ></iframe>
         </div>
       </div>
+
       <div className="flex items-center flex-col justify-center mt-20">
         <h1 className="text-9xl">Sachin Vishwakarma</h1>
         <h2 className="text-3xl mt-3">
@@ -27,70 +30,88 @@ function Hero() {
       </div>
       <div className="bg-black-500 w-full flex gap-28 justify-center mt-20">
         <div className="w-52 h-96 bg-neutral-500 rounded-md overflow-hidden">
-          <video
-            autoPlay 
-            loop
-            muted
-            className="h-screen w-full object-cover bg-gray-500"
-            src="src/assets/ShortVideos/Video1.mp4"
-          ></video>
-          <img className="mix-blend-multiply" src="src/assets/Phone.png" alt="" />
+          <iframe
+            className="w-full h-full"
+            src="https://www.youtube.com/embed/ZsenEZ-bGdE?si=UH1QN95ppED0ZhI1"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerpolicy="strict-origin-when-cross-origin"
+            allowfullscreen
+          ></iframe>
+          <img
+            className="mix-blend-multiply"
+            src="src/assets/Phone.png"
+            alt=""
+          />
         </div>
         <div className="w-52 h-96 bg-neutral-500 rounded-md overflow-hidden">
-          <video
-            autoPlay
-            loop
-            muted
-            className="w-full h-full object-cover"
-            src="src/assets/ShortVideos/Video2.mp4"
-          ></video>
+          <iframe
+            className="w-full h-full"
+            src="https://www.youtube.com/embed/8cEySbqBfbU?si=UUy3oMR3KWUHJhvn"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerpolicy="strict-origin-when-cross-origin"
+            allowfullscreen
+          ></iframe>
         </div>
         <div className="w-52 h-96 bg-neutral-500 rounded-md overflow-hidden">
-          <video
-            autoPlay
-            loop
-            muted
-            className="w-full h-full object-cover"
-            src="src/assets/ShortVideos/Video3.mp4"
-          ></video>
+          <iframe
+            className="w-full h-full"
+            src="https://www.youtube.com/embed/_QQPkueyBOo?si=G-P2T6Jq8Aj3EbeX"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerpolicy="strict-origin-when-cross-origin"
+            allowfullscreen
+          ></iframe>
         </div>
         <div className="w-52 h-96 bg-neutral-500 rounded-md overflow-hidden">
-          <video
-            autoPlay
-            loop
-            muted
-            className="w-full h-full object-cover"
-            src="src/assets/ShortVideos/Video4.mp4"
-          ></video>
+          <iframe
+            className="w-full h-full"
+            src="https://www.youtube.com/embed/WPRrNvqnk0E?si=8hZOoON6vD6pVjyS"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerpolicy="strict-origin-when-cross-origin"
+            allowfullscreen
+          ></iframe>
         </div>
       </div>
       <div className="bg-black-500 w-full flex justify-center gap-28 mt-20">
         <div className="w-52 h-96 bg-neutral-500 rounded-md overflow-hidden">
-          <video
-            autoPlay
-            loop
-            muted
-            className="w-full h-full object-cover"
-            src="src/assets/ShortVideos/Video5.mp4"
-          ></video>
+          <iframe
+            className="w-full h-full"
+            src="https://www.youtube.com/embed/dIPoAGenSNY?si=hWYmodLhI9xRuLUQ"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerpolicy="strict-origin-when-cross-origin"
+            allowfullscreen
+          ></iframe>
         </div>
         <div className="w-52 h-96 bg-neutral-500 rounded-md overflow-hidden">
-          <video
-            autoPlay
-            loop
-            muted
-            className="w-full h-full object-cover"
-            src="src/assets/ShortVideos/Video6.mp4"
-          ></video>
+          <iframe
+            className="w-full h-full"
+            src="https://www.youtube.com/embed/PB7fEOsYX0Q?si=6m_j9oLdHTLPsqNv"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerpolicy="strict-origin-when-cross-origin"
+            allowfullscreen
+          ></iframe>
         </div>
         <div className="w-52 h-96 bg-neutral-500 rounded-md overflow-hidden">
-          <video
-            autoPlay
-            loop
-            muted
-            className="w-full h-full object-cover"
-            src="src/assets/ShortVideos/Video7.mp4"
-          ></video>
+          <iframe
+            className="w-full h-full"
+            src="https://www.youtube.com/embed/7wItHGbQEfo?si=MkHmuGr_nbVYMDPy"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerpolicy="strict-origin-when-cross-origin"
+            allowfullscreen
+          ></iframe>
         </div>
       </div>
     </div>
--- src/Components/Longvideos.jsx
+++ src/Components/Longvideos.jsx
@@ -1,26 +1,106 @@
-import React from 'react'
+import React from "react";
 
 function Longvideos() {
   return (
-    <div className='w-full flex justify-center flex-wrap items-center gap-20 mt-20'>
-      <div className='w-2/5 h-64 bg-red-500 rounded-md overflow-hidden'>
-      <video autoPlay loop muted  className="w-full h-full object-cover" src="src/assets/LongVideos/Sequence1.mp4"></video></div>
-      <div className='w-2/5 h-64 bg-red-500 rounded-md overflow-hidden'>
-      <video autoPlay loop muted  className="w-full h-full object-cover" src="src/assets/LongVideos/Sequence2.mp4"></video></div>
-      <div className='w-2/5 h-64 bg-red-500 rounded-md overflow-hidden'>
-      <video autoPlay loop muted  className="w-full h-full object-cover" src="src/assets/LongVideos/Sequence3.mp4"></video></div>
-      <div className='w-2/5 h-64 bg-red-500 rounded-md overflow-hidden'>
-      <video autoPlay loop muted  className="w-full h-full object-cover" src="src/assets/LongVideos/Sequence4.mp4"></video></div>
-      <div className='w-2/5 h-64 bg-red-500 rounded-md overflow-hidden'>
-      <video autoPlay loop muted  className="w-full h-full object-cover" src="src/assets/LongVideos/Sequence5.mp4"></video></div>
-      <div className='w-2/5 h-64 bg-red-500 rounded-md overflow-hidden'>
-      <video autoPlay loop muted  className="w-full h-full object-cover" src="src/assets/LongVideos/Sequence6.mp4"></video></div>
-      <div className='w-2/5 h-64 bg-red-500 rounded-md overflow-hidden'>
-      <video autoPlay loop muted  className="w-full h-full object-cover" src="src/assets/LongVideos/Sequence7.mp4"></video></div>
-      <div className='w-2/5 h-64 bg-red-500 rounded-md overflow-hidden'>
-      <video autoPlay loop muted className="w-full h-full object-cover" src="src/assets/LongVideos/Sequence8.mp4"></video></div>
+    <div className="w-full flex justify-center flex-wrap items-center gap-20 mt-20">
+      <div className="w-2/5 h-64 bg-red-500 rounded-md overflow-hidden">
+        <iframe
+          className="w-full h-full"
+          src="https://www.youtube.com/embed/pJ3SOEgyPvQ?si=fG1MbHwCgXYEP-PL&autoplay=1&mute=1&loop=1&playlist=pJ3SOEgyPvQ"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerPolicy="strict-origin-when-cross-origin"
+          allowFullScreen
+        ></iframe>
+      </div>
+
+      <div className="w-2/5 h-64 bg-red-500 rounded-md overflow-hidden">
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/EAzg6xq0u5g?si=bQw8zvvy5ZA0yEpc"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerpolicy="strict-origin-when-cross-origin"
+          allowfullscreen
+        ></iframe>
+      </div>
+      <div className="w-2/5 h-64 bg-red-500 rounded-md overflow-hidden">
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/LTw2tlSmbPY?si=5y4oimHwgwqvMZVK"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerpolicy="strict-origin-when-cross-origin"
+          allowfullscreen
+        ></iframe>
+      </div>
+      <div className="w-2/5 h-64 bg-red-500 rounded-md overflow-hidden">
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/F4oHFOJbxaw?si=VME3RxATqfL4lT7E"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerpolicy="strict-origin-when-cross-origin"
+          allowfullscreen
+        ></iframe>
+      </div>
+      <div className="w-2/5 h-64 bg-red-500 rounded-md overflow-hidden">
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/NQF7gmvkMGM?si=l5MSNG7vUFoTRQo9"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerpolicy="strict-origin-when-cross-origin"
+          allowfullscreen
+        ></iframe>
+      </div>
+      <div className="w-2/5 h-64 bg-red-500 rounded-md overflow-hidden">
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/TejsoWLG0Nc?si=ogyRbijsetMnJuqM"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerpolicy="strict-origin-when-cross-origin"
+          allowfullscreen
+        ></iframe>
+      </div>
+      <div className="w-2/5 h-64 bg-red-500 rounded-md overflow-hidden">
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/cvnnCVZAXTA?si=Fj3BuycalBpWl8j8"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerpolicy="strict-origin-when-cross-origin"
+          allowfullscreen
+        ></iframe>
+      </div>
+      <div className="w-2/5 h-64 bg-red-500 rounded-md overflow-hidden">
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/4hNGeqSbAQI?si=PgKJeP1-TIRPkLt-"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerpolicy="strict-origin-when-cross-origin"
+          allowfullscreen
+        ></iframe>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Longvideos
+export default Longvideos;
